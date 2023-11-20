bde91bad91c60354181ab795c56ad63f427ef72a
Perelyn <64838956+Perelyn-sama@users.noreply.github.com>
2023-11-20 13:06:27 +0100

    delete files

--- Contract.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.19;
-
-interface IContract {
-function tokenURI(IERC721Metadata sablier, uint256 streamId) external view returns (string memory uri);function abbreviateAmount(uint256 amount, uint256 decimals) internal pure returns (string memory);function calculateDurationInDays(uint256 startTime, uint256 endTime) internal pure returns (string memory);function calculateStreamedPercentage(uint128 streamedAmount, uint128 depositedAmount);
-}
